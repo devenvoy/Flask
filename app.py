6ccacee7ec9001304708465beaf578db7994a5b6
devenvoy <iambeast0069@gmail.com>
2024-10-31 16:52:25 +0530

    crud operation created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for , redirect 
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 
@@ -15,9 +15,47 @@ class Task(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])  
 def index():
-    return render_template('index.html')
+
+    if(request.method == 'POST'):
+        task_content = request.form['content']
+        new_task = Task(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your task"
+    else:
+        tasks = Task.query.order_by(Task.date_created).all()
+        return render_template('index.html',tasks=tasks)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Task.query.get_or_404(id)
+
+    try:
+
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting that task"
+    
+@app.route('/update/<int:id>',methods=['GET','POST'])
+def update(id):
+    task = Task.query.get_or_404(id)
+    if request.method == 'POST':
+        try:
+            task.content = request.form['content']
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was a problem updating that task"
+    else:
+        return render_template('update.html',task=task)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5500,debug=True)
